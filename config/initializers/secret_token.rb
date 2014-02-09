22083e785f2b1d5ae46a474dae389a6b2c922851
rishi <rishi@joshsoftware.com>
2014-02-10 03:13:55 +0530

    set up an adorable cat fact and image page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdorableCatApp::Application.config.secret_token = '9343b218ba9c56cfdf708d4a6bef4d9e21624292c4e153668a89edcb344416430807e909e5842c7582155ef9ccb52f5ac92cc9f711fc8c49799179b2eb465ea7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AdorableCatApp::Application.config.session_store :cookie_store, key: '_adorable_cat_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AdorableCatApp::Application.config.session_store :active_record_store
